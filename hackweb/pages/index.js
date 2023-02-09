9580898c6b512f7b48bdec3a3ebbaa51640ac2a4
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-02-10 01:18:44 +0530

    new

--- hackweb/pages/index.js
+++ hackweb/pages/index.js
@@ -1,12 +1,9 @@
 import HomePage from './homepage';
-import TeamPage from './contactpage'
-import SignIn from '../components/google/signIn'
 export default function Home() {
 
   return (
    <>
     <HomePage/>
-    <SignIn/>
    </>
   )
 }
