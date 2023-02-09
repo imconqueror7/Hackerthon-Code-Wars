d69188bf5ea8a94e73e2041e4bc6ab217e2dc344
Nikhil <97503662+nikhilhvr@users.noreply.github.com>
2023-02-09 21:48:26 +0530

    firebase setup done

--- hackweb/pages/index.js
+++ hackweb/pages/index.js
@@ -1,11 +1,12 @@
 import HomePage from './homepage';
 import TeamPage from './contactpage'
+import SignIn from '../components/google/signIn'
 export default function Home() {
 
   return (
    <>
     <HomePage/>
-    
+    <SignIn/>
    </>
   )
 }
